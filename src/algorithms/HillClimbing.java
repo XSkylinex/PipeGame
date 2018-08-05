3cdeacb22559e91610764b18524cf59290c6c43c
Alex <alex11535@bezeqint.net>
2018-08-05 23:08:43 +0300

    day 3 23:07 add hillClimbing

--- src/algorithms/HillClimbing.java
+++ src/algorithms/HillClimbing.java
@@ -1,4 +1,54 @@
 package algorithms;
 
-public class HillClimbing {
+import Interfaces.Searchable;
+import Interfaces.Searcher;
+import classes.Solution;
+import classes.State;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+public class HillClimbing<T> implements Searcher<T> {
+
+    private Random random = new Random();
+    private long timeToRun=5000;
+
+    @Override
+    public Solution<T> Search(Searchable<T> s) {
+        //Define the current state as an initial state
+        State<T> next = s.getInitialState();
+        Solution<T> result = new Solution<>();
+        //System.out.println("HillClimbing");
+        long time0 = System.currentTimeMillis();
+        //Loop until the goal state is achieved or no more operators can be applied on the current state:
+        while (System.currentTimeMillis() - time0 < timeToRun) {
+
+            Collection<State<T>> neighbors = s.getAllPossibleStates(next);
+            if (Math.random() < 0.7&&!s.IsGoalState(next)) { // with a high probability
+                // find the best one
+                int grade = 0;
+                for (State<T> state : neighbors) {
+                    int g = (int)(state.getCost() *random.nextInt());
+                    if (g > grade) {
+                        grade = g;
+                        next = state;
+                        //add this step to the solution
+                        result.add(state);
+                    }
+                }
+            } else {
+                ArrayList<State<T>> states=new ArrayList<>(neighbors);
+                next = states.get(new Random().nextInt(neighbors.size()));
+            }
+        }
+        return result;
+
+    }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        // TODO Auto-generated method stub
+        return -1;
+    }
 }
--- src/classes/Solution.java
+++ src/classes/Solution.java
@@ -4,6 +4,11 @@ import java.util.Collection;
 
 public class Solution<T> {
 
+    public Solution() {
+    }
     public Solution(Collection<State<T>> arrayList) {
+    }
+
+    public void add(State<T> state) {
     }
 }
