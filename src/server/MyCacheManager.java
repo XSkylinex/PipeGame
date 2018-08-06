cdd9fabd5efc10c0af80e1d9c6703bb279de4f0c
Alex <alex11535@bezeqint.net>
2018-08-06 20:38:18 +0300

    day 4 20:37 almost done need to do tests

--- src/server/MyCacheManager.java
+++ src/server/MyCacheManager.java
@@ -2,22 +2,35 @@ package server;
 
 import classes.Solution;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class MyCacheManager<T> implements CacheManager<Solution<T>>{
+
     @Override
     public boolean isFileExist(String fileName) {
-        return false;
+        try {
+            Solution<T> solution = loadFile(fileName);
+            return (solution!=null&&
+                    solution.getSolution()!=null&&
+                    solution.getSolution().size()!=0);
+        } catch (Exception e) {
+           return false;
+        }
     }
 
     @Override
     public Solution<T> loadFile(String fileName) throws FileNotFoundException, IOException, ClassNotFoundException {
-        return null;
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName+".txt"));
+        Solution<T> solution= (Solution<T>)in.readObject();
+        in.close();
+        return solution;
     }
 
     @Override
-    public void saveFile(String fileName, Solution<T> file) throws IOException {
-
+    public void saveFile(String fileName, Solution<T> file) throws IOException { // from exercise 6 -b pincheta
+        ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream(fileName+".txt"));
+        out.writeObject(file);
+        out.flush();
+        out.close();
     }
 }
